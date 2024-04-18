be1fc381cad1cde5a182da1b185dd7e3f113edfa
danila <spook9400@mail.ru>
2024-04-18 09:39:38 +0300

    24.1 Вьюсеты и дженерики

--- users/models.py
+++ users/models.py
@@ -5,9 +5,13 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
+
     phone = models.CharField(max_length=20)
     city = models.CharField(max_length=100)
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
